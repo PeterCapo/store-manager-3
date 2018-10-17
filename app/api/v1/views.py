10324d4e73d6c728ef7cb9c2aab2664fbe6d18e0
peter okoth <pokoth2009@gmail.com>
2018-10-17 15:02:51 +0300

    [Chore #161281139] Add tests

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,15 +1,1 @@
-from flask_restful import Resource
-from flask import Flask, jsonify, make_response, request
 
-sales = []
-products = []
-
-
-class SpecificSale(Resource):
-    def get(self, id):
-        return make_response(jsonify(
-            {
-                'Status': "OK",
-                'Message': "Success",
-                'specific sale': sales[id]
-            }), 200)
