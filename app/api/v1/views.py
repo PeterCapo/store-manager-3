af11f591a1e58942089090a6129233cef703ea57
peter okoth <pokoth2009@gmail.com>
2018-10-17 17:14:44 +0300

    [Chore #161285784] Add travis configuration file

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -23,3 +23,75 @@ class SpecificProduct(Resource):
                 'Message': "Success",
                 'specific product': products[id]
             }), 200)
+
+
+class Products(Resource):
+    def get(self):
+        return make_response(jsonify(
+            {
+                'Status': "Ok",
+                'Message': "Success",
+                'My Products': products
+            }), 200)
+
+    def post(self):
+        data = request.get_json()
+        id = len(products) + 1
+        productName = data['Product Name']
+        category = data['Category']
+        stockBalance = data['Stock Balance']
+        minStockBalance = data['Minimum Inventory']
+        price = data['Price']
+
+        payload = {
+            'id': id,
+            'Description': {
+                'Product Name': productName, 'Stock Balance': stockBalance,
+                'Minimum Inventory': minStockBalance,
+                'Category': category
+                },
+            'Price': price
+        }
+        products.append(payload)
+
+        return make_response(jsonify(
+            {
+                'Message': 'Product created',
+                'status': 'ok',
+                'Data': products
+            }), 201)
+
+
+class Sales(Resource):
+
+    def get(self):
+        return make_response(jsonify(
+            {
+                'Status': "Ok",
+                'Message': "Success",
+                'My Sales': sales
+            }), 200)
+
+    def post(self):
+        data = request.get_json()
+        id = len(sales) + 1
+        itemName = data['Item Name']
+        attendant = data['Attendant']
+        quantity = data['Quantity']
+        price = data['Price']
+
+        payload = {
+            'id': id,
+            'Item Name': itemName,
+            'Attendant': attendant,
+            'Quantity': quantity,
+            'Price': price
+        }
+        sales.append(payload)
+
+        return make_response(jsonify(
+            {
+                'Message': 'Sales created',
+                'status': 'ok',
+                'Data': sales
+            }), 201)
