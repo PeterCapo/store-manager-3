6b719f97f84a39a875858f4df9eb5ddd0c74f754
peter okoth <pokoth2009@gmail.com>
2018-10-17 17:23:20 +0300

    [Chore #161285784] Add travis configuration file

---
+++ instance/config.py
@@ -0,0 +1,29 @@
+import os
+
+
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    DATABASE_URL = os.getenv("DATABASE_TEST_URL")
+
+
+class ReleaseConfig(Config):
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'release': ReleaseConfig,
+}
